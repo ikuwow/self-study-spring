0b31fa54df1f89e171c9954851f634194599bf05
Ikuo Degawa <degawa@ikuwow.com>
2017-10-23 12:48:15 +0900

    Initial commit

---
+++ src/main/java/com/ikuwow/selfstudyspring/SelfstudyspringApplication.java
@@ -0,0 +1,12 @@
+package com.ikuwow.selfstudyspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelfstudyspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SelfstudyspringApplication.class, args);
+	}
+}
---
+++ src/test/java/com/ikuwow/selfstudyspring/SelfstudyspringApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ikuwow.selfstudyspring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SelfstudyspringApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
