8adbb31cc19ea3d8202336d51bc514c4d1ff0fbb
Ikuo Degawa <degawa@ikuwow.com>
2017-10-24 00:35:25 +0900

    Add post endpoint

--- src/main/java/com/ikuwow/selfstudyspring/controller/page/MemoController.java
+++ src/main/java/com/ikuwow/selfstudyspring/controller/page/MemoController.java
@@ -1,8 +1,11 @@
 package com.ikuwow.selfstudyspring.controller.page;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
+import com.ikuwow.selfstudyspring.model.Memo;
 
 import java.util.List;
 import java.util.Map;
@@ -12,12 +15,22 @@ import java.util.HashMap;
 @Controller
 @RequestMapping("memo")
 public class MemoController {
+
     @RequestMapping("")
     public String get(Model model) {
         List<Map<String, Object>> items = new ArrayList<>();
         Map<String, Object> item = new HashMap<>();
         item.put("memo", "Empty Memo");
         item.put("author", "Empty Author");
+        items.add(item);
+
+        model.addAttribute("items", items);
+        return "memo";
+    }
+
+    @RequestMapping(value="", method=RequestMethod.POST)
+    public String post(@ModelAttribute Memo item, Model model) {
+        List<Memo> items = new ArrayList<>();
         items.add(item);
 
         model.addAttribute("items", items);
